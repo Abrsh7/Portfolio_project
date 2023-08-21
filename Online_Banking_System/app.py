b18066f675220fd9697101ecfc36b0bdba7412f8
abrsh7 <abrsh127n@gmail.com>
2023-08-21 22:33:35 +0300

    app

--- Online_Banking_System/app.py
+++ Online_Banking_System/app.py
@@ -1,30 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return('hello bank')
+    return render_template('homepage.html')
 
 @app.route('/login')
 def login():
-    return('login page')
+    return render_template('login.html')
 
 @app.route('/register')
 def register():
-    return('register page')
+    return render_template('register.html')
 
 @app.route('/deposite')
 def deposite():
-    return('deposite page')
+    return render_template('deposit.html')
 
 @app.route('/withdraw')
 def withdraw():
-    return('withdraw page')
+    return render_template('withdraw.html')
 
 @app.route('/logout')
 def logout():
-    return('logout page')
+    return redirect(url_for('homepage'))
 
 
 if __name__ == "__main__":
